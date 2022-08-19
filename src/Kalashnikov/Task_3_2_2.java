2e18436a4bdcb4e2fe2297908d37ccebcc158211
Temo4kin <temo4kinn@gmail.com>
2022-08-19 19:52:10 +0300

    Вторая попытка решить задание 2 к лекции 3

---
+++ src/Kalashnikov/Task_3_2_2.java
@@ -0,0 +1,15 @@
+package Kalashnikov;
+
+import java.util.Scanner;
+
+public class Task_3_2_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int side_of_square = sc.nextInt();
+        int radius_of_circle = sc.nextInt();
+        sc.close();
+
+        System.out.println(side_of_square);
+    }
+}
