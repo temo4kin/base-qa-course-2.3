5d1aec233d756694932d31d1627ab683bd9f7ca7
Temo4kin <temo4kinn@gmail.com>
2022-08-19 19:45:32 +0300

    Решение задания 2 к Лекции 3 (не работает)

--- src/Kalashnikov/Task_3_2.java
+++ src/Kalashnikov/Task_3_2.java
@@ -1,7 +1,22 @@
 package Kalashnikov;
 
+import java.util.Scanner;
 public class Task_3_2 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int side_of_square = sc.nextInt();
+        int radius_of_circle = sc.nextInt();
+        sc.close();
+
+        int area_of_square = side_of_square * side_of_square;
+        double area_of_circle = Math.PI * (radius_of_circle * radius_of_circle);
+
+        if (area_of_square > area_of_circle){
+            System.out.println("Площадь квадрата больше площади круга");
+        }
+        else
+        {
+            System.out.println("Площадь круга больше площади квадрата");
+        }
     }
 }
