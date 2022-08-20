486339bab07ef4f97a45198865215f928a1d91e0
Temo4kin <temo4kinn@gmail.com>
2022-08-20 16:57:17 +0300

    Добавление дополнительных проверок в решение

--- src/Kalashnikov/Task_3_from_3.java
+++ src/Kalashnikov/Task_3_from_3.java
@@ -6,12 +6,26 @@ public class Task_3_from_3 {
         int number = sc.nextInt();
         sc.close();
 
-        int units = number % 10;
-        int decimals = number/10;
+        if (number >= 10 && number < 100)
+        {
+            int units = number % 10;
+            int decimals = number/10;
 
-        System.out.println(units%decimals);
-        System.out.println(decimals%units);
+            if (units == 0)
+            {
+                System.out.println("Äåëèòü íà íîëü íåëüçÿ!");
+            }
 
+            else
+            {
+                System.out.println(units % decimals);
+                System.out.println(decimals % units);
+            }
+        }
 
+        else
+        {
+            System.out.println("Âû ââåëè íå äâóçíà÷íîå ÷èñëî!");
+        }
     }
 }
